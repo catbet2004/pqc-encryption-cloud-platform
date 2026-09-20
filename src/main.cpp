6565947e3f20cc2731cee6ccde05989a2f61014d
catbet2004 <catbet2004@gmail.com>
2026-09-20 15:33:55 -0400

    New test for ML-DSA added

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "algos/MLKEM.hpp"
+#include "algos/MLDSA.hpp"
+
 
 int main(){
     try{
@@ -23,7 +27,38 @@ int main(){
             return 1;
         }
 
-        std::cout<<"Shared secret size: "<< userSecret.size()<< " bytes\n";
+        std::cout<<"Shared secret size: "<< userSecret.size()<< " bytes\n\n";
+
+        MLDSA signer;
+
+        std::cout<<"ML_DSA-65 keypair generated.\n";
+
+        std::string text="Post-quantum secure cloud";
+
+        std::vector<unsigned char> message(text.begin(), text.end());
+
+        auto signature=signer.sign(message);
+
+        std::cout << "Message signed successfully.\n";
+
+        if(signer.verify(message,signature)){
+            std::cout<<"SUCCESS: Signature is valid!\n";
+
+        } else{
+            std::cout<<"FAILURE: Signature is invalid.\n";
+        }
+
+        //tampering test
+
+        std::string changedText="Post-qunatum insecure cloud";
+
+        std::vector<unsigned char> changedMessage(changedText.begin(), changedText.end());
+
+        if(signer.verify(changedMessage, signature)){
+            std::cout<<"WARNING: Tampered message passed!\n";
+        } else{
+            std::cout<< "SUCCESS: Tampering detected!\n";
+        }
 
     } catch(const std::exception& error){
         std::cerr << "Error: " << error.what() << '\n';
